65ead0b13633eafc1920d6403ab150e6ba9fd546
Juan <juanhurtado0405@gmaail.com>
2021-02-03 17:40:46 -0500

    Unit testing

--- app/src/test/java/com/clearmind/animeland/login/LoginViewModelTest.kt
+++ app/src/test/java/com/clearmind/animeland/login/LoginViewModelTest.kt
@@ -1,70 +1,109 @@
 package com.clearmind.animeland.login
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.clearmind.animeland.CoroutineTestRule
+import com.clearmind.animeland.LifeCycleTestOwner
+import com.clearmind.animeland.MainCoroutineScopeRule
+import com.clearmind.animeland.TestCoroutineRule
+import com.clearmind.animeland.core.di.Either
+import com.clearmind.animeland.model.auth.ProfileModel
+import com.clearmind.animeland.usecase.AuthUseCase
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.Mockito.`when` as whenever
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class LoginViewModelTest {
-    private var viewModel: LoginViewModel? = null
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
+
+    private lateinit var loginViewModel: LoginViewModel
 
     @Mock
-    private val repository: GitHubRepository? = null
+    private lateinit var getAuthUseCase: AuthUseCase
+
+    @Mock
+    private lateinit var mAuth: FirebaseAuth
+
+    @Mock
+    private lateinit var authCredential: AuthCredential
+
+    @Mock
+    private lateinit var authObserver: Observer<LoginViewModel.LoginState>
+
+    @Captor
+    private lateinit var captor: ArgumentCaptor<LoginViewModel.LoginState>
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
-        MockitoAnnotations.initMocks(this) // required for the "@Mock" annotations
-
-        // Make viewModel a mock while using mock repository and viewContract created above
-        viewModel = Mockito.spy(SearchViewModel(repository))
+        //MockitoAnnotations.initMocks(this) // required for the "@Mock" annotation
+        loginViewModel = LoginViewModel(getAuthUseCase)
     }
 
     @Test
-    fun searchGitHubRepos_noQuery() {
-        val searchQuery: String? = null
+    fun `when calling for authentication then return loading`() {
+        testCoroutineRule.runBlockingTest {
+            loginViewModel.authenticatedUserLiveData.observeForever(authObserver)
+            loginViewModel.signInWithMultiplesCredentials(authCredential)
+            verify(authObserver).onChanged(LoginViewModel.LoginState.LOADING)
+        }
+    }
 
-        // Trigger
-        viewModel.searchGitHubRepos(searchQuery)
 
-        // Validation
-        Mockito.verify(repository, Mockito.never()).searchRepos(searchQuery, viewModel)
+    @Test
+    fun `when response results ok then return an profile object successfully`() {
+        val model = ProfileModel("abc@x.y")
+        testCoroutineRule.runBlockingTest {
+            loginViewModel.authenticatedUserLiveData.observeForever(authObserver)
+            whenever(getAuthUseCase.execFirebaseSignIn(authCredential)).thenAnswer{
+                Either.Right(LoginViewModel.LoginState.Success(model))
+            }
+            loginViewModel.signInWithMultiplesCredentials(authCredential)
+            assertNotNull(loginViewModel.getLiveProfile().value)
+            assertEquals(LoginViewModel.LoginState.Success(model), loginViewModel.getLiveProfile().value)
+        }
     }
 
     @Test
-    fun searchGitHubRepos() {
-        val searchQuery = "some query"
-
-        // Trigger
-        viewModel.searchGitHubRepos(searchQuery)
-
-        // Validation
-        Mockito.verify(repository, Mockito.times(1)).searchRepos(searchQuery, viewModel)
+    fun `when response results fails then return an normal error`() {
+        val exception = AuthUseCase.GetAuthFailure.FirebaseSignIn()
+        testCoroutineRule.runBlockingTest {
+            loginViewModel.authenticatedUserLiveData.observeForever(authObserver)
+            whenever(getAuthUseCase.execFirebaseSignIn(authCredential)).thenAnswer {
+                Either.Left(exception)
+            }
+            loginViewModel.signInWithMultiplesCredentials(authCredential)
+            assertNotNull(loginViewModel.getLiveProfile().value)
+            assertEquals(LoginViewModel.LoginState.Error(exception), loginViewModel.getLiveProfile().value)
+        }
     }
 
-    @Test
-    fun handleGitHubResponse_Success() {
-        val response: Response = Mockito.mock(Response::class.java)
-        val searchResponse: SearchResponse = Mockito.mock(SearchResponse::class.java)
-        Mockito.doReturn(true).`when`(response).isSuccessful()
-        Mockito.doReturn(searchResponse).`when`(response).body()
-        val searchResults: List<SearchResult> = Collections.singletonList(SearchResult())
-        Mockito.doReturn(searchResults).`when`(searchResponse).getSearchResults()
-
-        // Trigger
-        viewModel.handleGitHubResponse(response)
-
-        // Validation
-        Mockito.verify(viewModel, Mockito.times(1)).renderSuccess(searchResponse)
-    }
-
-    @Test
-    fun renderSuccess() {
-        val response: Response = Mockito.mock(Response::class.java)
-        val searchResponse: SearchResponse = Mockito.mock(SearchResponse::class.java)
-        Mockito.doReturn(true).`when`(response).isSuccessful()
-        Mockito.doReturn(searchResponse).`when`(response).body()
-        Mockito.doReturn(1001).`when`(searchResponse).getTotalCount()
-
-        // Trigger
-        viewModel.handleGitHubResponse(response)
-
-        // Validation
-        Assert.assertEquals("Number of results: 1001", viewModel.status.get())
+    @After
+    fun tearDown(){
+        loginViewModel.authenticatedUserLiveData.removeObserver(authObserver)
     }
 }
